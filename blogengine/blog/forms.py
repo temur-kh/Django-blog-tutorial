899bfef9b9d9386ce80399b39561f4360813a462
Temur <tim_1998@bk.ru>
2018-09-23 00:11:07 +0300

    Blog Website on Django

---
+++ blogengine/blog/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from .models import *
+from django.core.exceptions import ValidationError
+
+
+class TagForm(forms.ModelForm):
+    # title = forms.CharField(
+    #     max_length=50
+    # )
+    #
+    # slug = forms.SlugField(
+    #     max_length=50
+    # )
+    #
+    # title.widget.attrs.update({'class': 'form-control'})
+    # slug.widget.attrs.update({'class': 'form-control'})
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        return new_slug
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        return new_slug
